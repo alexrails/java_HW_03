a58fd4a19e6fb02872291820b1a360cad21c75d1
Alex <alexrails@yandex.ru>
2021-03-25 22:35:10 +0300

    First task

---
+++ src/com/company/Main.java
@@ -0,0 +1,20 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// First task
+        int month = 0;
+        if (month == 1 || month == 2 || month == 12){
+            System.out.println("Winter");
+        } else if(month >= 3 && month <= 5){
+            System.out.println("Spring");
+        } else if(month >= 6 && month <= 8){
+            System.out.println("Summer");
+        } else if(month >= 9 && month <= 11){
+            System.out.println("Fall");
+        } else {
+            System.out.println("Wrong number");
+        }
+    }
+}
