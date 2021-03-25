eabce2fc8e02feddbdc0d46ea8fbdaf8c9077840
Alex <alexrails@yandex.ru>
2021-03-25 23:07:13 +0300

    Second task

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,8 +3,8 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// First task
-        int month = 0;
+	    // First task
+        int month = 8;
         if (month == 1 || month == 2 || month == 12){
             System.out.println("Winter");
         } else if(month >= 3 && month <= 5){
@@ -15,6 +15,49 @@ public class Main {
             System.out.println("Fall");
         } else {
             System.out.println("Wrong number");
+       }
+
+        // Second task
+        switch(month){
+            case 1:
+                System.out.println("Winter");
+                break;
+            case 2:
+                System.out.println("Winter");
+                break;
+             case 3:
+                 System.out.println("Spring");
+                 break;
+            case 4:
+                System.out.println("Spring");
+                break;
+            case 5:
+                System.out.println("Spring");
+                break;
+            case 6:
+                System.out.println("Summer");
+                break;
+            case 7:
+                System.out.println("Summer");
+                break;
+            case 8:
+                System.out.println("Summer");
+                break;
+            case 9:
+                System.out.println("Fall");
+                break;
+            case 10:
+                System.out.println("Fall");
+                break;
+            case 11:
+                System.out.println("Fall");
+                break;
+            case 12:
+                System.out.println("Winter");
+                break;
+            default:
+                System.out.println("Wrong number");
         }
     }
+
 }
